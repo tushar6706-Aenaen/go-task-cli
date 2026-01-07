3dddf3aed773e01d9b0eee483703ab5cc2f8bf16
.env <chauhantushar6706@gmail.com>
2026-01-08 00:56:28 +0530

    completed

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
-
 )
 
 func readTasks() []string {
-	data, err := os.ReadFile("tasks,txt")
+	data, err := os.ReadFile("tasks.txt")
 	if err != nil {
 		return []string{}
 	}
@@ -33,27 +33,56 @@ func main() {
 		}
 		task := os.Args[2]
 
-		file , _ := os.OpenFile(
-			"texts.txt",
+		file, _ := os.OpenFile(
+			"tasks.txt",
 			os.O_APPEND|os.O_CREATE|os.O_WRONLY,
 			0644,
 		)
 		defer file.Close()
 
-		file.WriteString("[]"+ task + "\n")
+		file.WriteString("[ ] " + task + "\n")
 		fmt.Println("Task added ✅")
 
 	case "list":
-		tasks:=readTasks()
+		tasks := readTasks()
 
-		for i , task := range tasks{
+		for i, task := range tasks {
 			if task != "" {
-				fmt.Printf("%d. %s\n",i+1,task)
+				fmt.Printf("%d. %s\n", i+1, task)
 			}
 		}
 	case "done":
+		if len(os.Args) < 3 {
+			fmt.Println("Please provide task number")
+			return
+		}
 
-		fmt.Println("Done command selected")
+		taskNum, err := strconv.Atoi(os.Args[2])
+
+		if err != nil {
+			fmt.Println("Invalid task number")
+			return
+		}
+
+		tasks := readTasks()
+
+		if taskNum <=0 || taskNum > len(tasks){
+			fmt.Println("Task not found")
+			return 
+		}
+
+		tasks[taskNum-1] = strings.Replace(
+			tasks[taskNum-1],
+			"[ ]",
+			"[x]",
+			1,
+		)
+
+		output := strings.Join(tasks,"\n")
+		os.WriteFile("tasks.txt",[]byte(output),0644)
+
+		fmt.Println("Task marked as done ✅")
+
 	default:
 		fmt.Println("Unknown command")
 	}
