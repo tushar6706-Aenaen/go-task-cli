fb8d682a6622dfd8c3498fee6ce3a0c5dba2172e
.env <chauhantushar6706@gmail.com>
2026-01-07 22:23:20 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage : task add|list|done")
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "add":
+		if len(os.Args) < 3 {
+			fmt.Println("Please provide a task")
+			return
+		}
+		task := os.Args[2]
+
+		file, _ := os.OpenFile("tasks.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		defer file.Close()
+
+		file.WriteString(task + "\n")
+		fmt.Println("Task added ✅")
+
+	case "list":
+
+		data,_ :=os.ReadFile("tasks.txt")
+		fmt.Println(string(data))
+	case "done":
+		
+		fmt.Println("Done command selected")
+	default:
+		fmt.Println("Unknown command")
+	}
+
+}
